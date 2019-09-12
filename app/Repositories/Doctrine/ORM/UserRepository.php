4c3c810ebd6d799a720cdd5a43961f80b258d8eb
AlbertLabarentoJr <albert.labarentojr@flexisourceit.com.au>
2019-09-12 12:05:13 +0800

    master
    - Added repository test

--- app/Repositories/Doctrine/ORM/UserRepository.php
+++ app/Repositories/Doctrine/ORM/UserRepository.php
@@ -1,10 +1,9 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Repositories\Doctrine;
+namespace App\Repositories\Doctrine\ORM;
 
 use App\Database\Entities\User;
-use App\Repositories\Doctrine\ORM\AbstractRepository;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 
 final class UserRepository extends AbstractRepository implements UserRepositoryInterface
